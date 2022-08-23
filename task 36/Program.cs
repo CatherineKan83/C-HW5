ba97fcecd0e5a12ccfc66a26c3025c421c777d77
Catherine <kankatya@bk.ru>
2022-08-23 18:04:48 +0500

    Added task 36

---
+++ task 36/Program.cs
@@ -0,0 +1,42 @@
+﻿void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for(int i=0; i<arr.GetLength(0); i++)
+    {
+        Console.Write($"{arr[i]}");
+        if(i<arr.GetLength(0)-1)
+        {
+            Console.Write(",");
+        }
+    }
+Console.WriteLine("]");
+}
+void FillArray(int[]arr)
+{
+    for(int i=0;i<arr.GetLength(0); i++)
+    {
+        arr[i] = new Random().Next(-10,100);
+    }
+Console.WriteLine();
+}
+void CountSum(int[]arr)
+{
+    int count = 0;
+    for(int i=0;i<arr.GetLength(0);i++)
+    {
+        if(i%2!=0)
+        {
+            count += arr[i];
+        }
+    }
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях массива: {count} ");    
+}
+Console.Write("Введите размер массива: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[c];
+FillArray(array);
+PrintArray(array);
+CountSum(array);
+
+
+
