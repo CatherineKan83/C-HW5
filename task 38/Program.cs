37174b4955f056cfb30ab603ee4b4d4751b54037
Catherine <kankatya@bk.ru>
2022-08-24 18:00:15 +0500

    Added task 38

---
+++ task 38/Program.cs
@@ -0,0 +1,33 @@
+﻿void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for(int i=0; i<arr.GetLength(0); i++)
+    {
+        Console.Write($"{arr[i]}");
+        if(i<arr.GetLength(0)-1)
+        {
+            Console.Write(",");
+        }
+    }
+Console.WriteLine("]");
+}
+void FillArray(int[]arr)
+{
+    for(int i=0;i<arr.GetLength(0); i++)
+    {
+        arr[i] = new Random().Next(-10,90);
+    }
+Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[c];
+FillArray(array);
+PrintArray(array);
+int dif = array.Max() - array.Min();
+Console.WriteLine($"Максимальным элементом является [{array.Max()}], минимальным [{array.Min()}]. Разница между ними составляет {dif}. ");  
+
+
+
+
